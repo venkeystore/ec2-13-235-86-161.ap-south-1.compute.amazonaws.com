b8c04587bb3318b325f83bfccc3c07d671b6bce4
Venkatesh <Venkateswara.Mukkamalla@INFOVISIONLABS.COM>
2020-01-16 13:21:50 +0530

    add react

---
+++ precache-manifest.c135db178bc96c9e8b401583918adb85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7d06c10e21108a3012dcb9955c9350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dde9bb6124baad146eed",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "236160974d55546a88a1",
+    "url": "/static/css/main.63ab0218.chunk.css"
+  },
+  {
+    "revision": "dde9bb6124baad146eed",
+    "url": "/static/js/2.c6678360.chunk.js"
+  },
+  {
+    "revision": "236160974d55546a88a1",
+    "url": "/static/js/main.2f2bfdef.chunk.js"
+  },
+  {
+    "revision": "4971552d781bb9f613b6",
+    "url": "/static/js/runtime-main.b9e92637.js"
+  }
+]);
